1c889848a78ad6d2135712b2e695fc197deb576c
Kamil <rytel.kamil@gmail.com>
2018-02-22 01:53:12 +0000

    Add base for data saving

--- hub/connection/dockingStation.go
+++ hub/connection/dockingStation.go
@@ -2,6 +2,7 @@ package connection
 
 import (
 	"SensorHub/hub/dataReading"
+	"SensorHub/hub/processing"
 	"bufio"
 	"io"
 	"log"
@@ -32,9 +33,7 @@ func Open(addr, port string) {
 
 func handle(conn net.Conn) {
 	reader := bufio.NewReader(conn)
-
 	for {
-		log.Println("Reading from", conn.RemoteAddr().String())
 		msg, err := reader.ReadString('\n')
 		if err != nil {
 			if err == io.EOF {
@@ -47,8 +46,13 @@ func handle(conn net.Conn) {
 		}
 
 		msg = strings.TrimSpace(msg)
-		go dataReading.Parse(msg)
-		log.Println("Message from", conn.RemoteAddr().String(), "'"+msg+"' passed to the data reading handler...")
+		log.Println("Read from", conn.RemoteAddr().String()+":", msg)
+		reading, err := dataReading.Parse(msg)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
 
+		processing.SaveDataReading(reading)
 	}
 }
---
+++ hub/processing/processing.go
@@ -0,0 +1,7 @@
+package processing
+
+import "SensorHub/hub/dataReading"
+
+func SaveDataReading(data dataReading.DataReading) {
+
+}
