322be758c5c261b7151e79d31213bb0c80762c0c
Kamil <rytel.kamil@gmail.com>
2018-07-02 00:35:27 +0100

    add config printing

--- station/main.py
+++ station/main.py
@@ -4,12 +4,22 @@ import json
 
 def main():
     ip, port, buffSize = loadConfig()
+    printConfig(ip, port, buffSize)
     
     s = Station(ip, port, buffSize)
-    s.echo("hi")
+    # s.echo("hi")
 
     # ts = TempSensor()
     # print(ts.read())
+
+    
+def printConfig(ip, port, buffSize):
+    print("""Configuration:
+    ip:\t\t{}
+    port:\t{}
+    buffSize:\t{}
+    """.format(ip, port, buffSize))
+
 
 def loadConfig():
     with open('config.json', 'r') as f:
@@ -20,6 +30,7 @@ def loadConfig():
     buffSize = config['station']['buffSize']
 
     return ip, port, buffSize
+
 
 if __name__ == '__main__':
     main()
