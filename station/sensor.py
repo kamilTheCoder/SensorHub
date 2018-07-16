aa38751f2849c14fe9231d8fcb2d75859290b19f
Kamil <rytel.kamil@gmail.com>
2018-07-16 23:07:03 +0100

    fix import and initialiser issues

--- station/sensor.py
+++ station/sensor.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-import sensor.dht11 as dht11
+import dht11
 
 class Sensor:
     def __init__(self, gpio):
@@ -13,7 +13,7 @@ class TempSensor(Sensor):
     instance = None
 
     def __init__(self, gpio):
-        super().__init__(self, gpio)
+        super().__init__(gpio)
         print("Instanciating DHT11...")
         self.instance = dht11.DHT11(pin=self.gpio)
 
