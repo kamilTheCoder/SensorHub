f8fe419094dd848f2d4f3a931a080c7e2d473bd6
Kamil <rytel.kamil@gmail.com>
2018-07-16 23:06:21 +0100

    Add simple DHT11 handling

--- station/station.py
+++ station/station.py
@@ -1,16 +1,19 @@
 import RPi.GPIO as GPIO
-import sensor.sensor as sensor
+from sensor import TempSensor
 import socket
 import json
 
 class Station:
+
+    dht11 = None
+
     def __init__(self):
         self.ip = "127.0.0.1"
         self.port = 1984
         self.bufferSize = 1024
 
-        self.ip, self.port, self.bufferSize = self.loadConfig()
-
+        self.ip, self.port, self.bufferSize, self.sensors = self.loadConfig()
+        
         self.initGpio()
 
     def sendData(self, message):
@@ -27,6 +30,7 @@ class Station:
         print(msg)
 
     def loadConfig(self):
+        print("Loading configuration...")
         with open('config.json', 'r') as f:
             config = json.load(f)
 
@@ -34,10 +38,22 @@ class Station:
         port = config['station']['port']
         buffSize = config['station']['buffSize']
 
-        return ip, port, buffSize
+        sensors = []
+        for sensor in config['sensors']:
+            name = sensor['name']
+            pin = sensor['pin']
+            sensors.append((name, pin))
+
+        return ip, port, buffSize, sensors
 
     def printConfig(self):
-        print("configuration:\nip:\t\t{}\nport:\t\t{}\nbuffSize:\t{}".format(self.ip, self.port, self.bufferSize))
+        print("configuration:")
+        print("\tip:\t\t{}".format(self.ip))
+        print("\tport:\t\t{}".format(self.port))
+        print("\tbuffSize:\t{}".format(self.bufferSize))
+        print("sensors:")
+        for s in self.sensors:
+            print("\tname: {}\tpin: {}".format(s[0], s[1]))
 
     def initGpio(self):
         print("Initialising GPIO...")
@@ -45,5 +61,18 @@ class Station:
         GPIO.setmode(GPIO.BCM)
         GPIO.cleanup()
 
-    def initDht11(self, gpio):
-        self.dht11 = sensor.TempSensor(gpio)
+    def initDht11(self):    
+        print("Initialising DHT11...") 
+        self.dht11 = TempSensor(self.sensors[0][1])
+
+    def readDht11(self):
+        if self.dht11 is None:
+            print("Warning: no DHT11 found")
+            return
+        
+        #print("Reading DHT11...")  
+        read = self.dht11.read()
+        print(read)
+        return read
+
+
