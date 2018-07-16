a3951f3cd4d5b1a2b5b457e4a4ad2b7a88f3ae3f
Kamil <rytel.kamil@gmail.com>
2018-07-17 00:27:28 +0100

    add sensor information and type to the Sensor class

--- station/sensor.py
+++ station/sensor.py
@@ -2,18 +2,22 @@ import RPi.GPIO as GPIO
 import dht11
 
 class Sensor:
-    def __init__(self, gpio):
+    def __init__(self, gpio, name):
         self.gpio = gpio
+        self.name = name
     
     def read(self):
         return None
 
+    def getInfo(self):
+        return (self.name, self.gpio)
 
-class TempSensor(Sensor):
+
+class Dht11Sensor(Sensor):
     instance = None
 
     def __init__(self, gpio):
-        super().__init__(gpio)
+        super().__init__(gpio, 'DHT11')
         print("Instanciating DHT11...")
         self.instance = dht11.DHT11(pin=self.gpio)
 
