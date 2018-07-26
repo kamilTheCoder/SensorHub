64c1915ba18b399997e4dbaa320b7614ae16ef8a
Kamil <rytel.kamil@gmail.com>
2018-07-26 23:56:23 +0100

    make use of mysql handling

--- station/main.py
+++ station/main.py
@@ -6,22 +6,24 @@ def main():
     s = station.Station()
     s.printConfig()
 
-    retries = 0
-    maxRetries = 10
-    print("Attempting to read...")
-    while retries < maxRetries:        
-        result = s.readDht11()
+    s.registerReading()
 
-        if result != None and result.is_valid():
-            retries = 0
-            print("Data read @ " + str(datetime.datetime.now()))
-            print("\tTemperature: %d C" % result.temperature)
-            print("\tHumidity: %d %%" % result.humidity)
-        else:
-            retries += 1
+    # retries = 0
+    # maxRetries = 10
+    # print("Attempting to read...")
+    # while retries < maxRetries:        
+    #     result = s.readDht11()
+
+    #     if result != None and result.is_valid():
+    #         retries = 0
+    #         print("Data read @ " + str(datetime.datetime.now()))
+    #         print("\tTemperature: %d C" % result.temperature)
+    #         print("\tHumidity: %d %%" % result.humidity)
+    #     else:
+    #         retries += 1
         
-        time.sleep(5)
-    print("Finished reading after 10 failed retries")
+    #     time.sleep(5)
+    # print("Finished reading after 10 failed retries")
 
 
 if __name__ == '__main__':
